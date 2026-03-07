18cbf40408d3d2c8d78149dd1fa401f9a9e547d2
Nusret Memic <nmemic@hotmail.com>
2026-03-08 00:10:22 +0100

    Simplify formula to use pipx for installation

--- Formula/macmaint.rb
+++ Formula/macmaint.rb
@@ -1,19 +1,18 @@
 class Macmaint < Formula
-  include Language::Python::Virtualenv
-
   desc "AI-powered CLI maintenance agent for macOS"
   homepage "https://github.com/nusretmemic/macmaint"
   url "https://github.com/nusretmemic/macmaint/archive/refs/tags/v0.1.0.tar.gz"
   sha256 "433e234bb2edffb6ba11f66fdc8e7aeebc616e2cc6925b7e78798fa87d42f2be"
   license "MIT"
 
-  depends_on "python@3.12"
+  depends_on "pipx"
 
   def install
-    virtualenv_create(libexec, "python3.12")
-    system libexec/"bin/pip", "install", "--upgrade", "pip", "setuptools", "wheel"
-    system libexec/"bin/pip", "install", "."
-    bin.install_symlink libexec/"bin/macmaint"
+    system "pipx", "install", "--force", "git+https://github.com/nusretmemic/macmaint.git@v0.1.0"
+  end
+
+  def post_install
+    system "pipx", "ensurepath"
   end
 
   def caveats
@@ -23,10 +22,12 @@ class Macmaint < Formula
       2. Provide your OpenAI API key when prompted
 
       Configuration will be stored in: ~/.macmaint/
+      
+      Note: You may need to restart your terminal for the macmaint command to be available in your PATH.
     EOS
   end
 
   test do
-    system "#{bin}/macmaint", "--help"
+    system "macmaint", "--help"
   end
 end
